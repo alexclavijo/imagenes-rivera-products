51f051208f36c7211b537dd59d0bab1834f58713
Alejandro Clavijo <Alejandro.Clavijo@fsresidential.com>
2019-06-21 20:45:21 -0400

    ORDER PAGE

--- ImagenesRivera.Products.KeyChain/src/app/@order/order.component.ts
+++ ImagenesRivera.Products.KeyChain/src/app/@order/order.component.ts
@@ -1,5 +1,6 @@
+import { IUser } from './../models/user';
 import { Component, OnInit } from '@angular/core';
-import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import { IPayPalConfig, ICreateOrderRequest, IPayer } from 'ngx-paypal';
 
 @Component({
   selector: 'app-order',
@@ -8,19 +9,73 @@ import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 })
 export class OrderComponent implements OnInit {
 
-  isLinear = false;
-  firstFormGroup: FormGroup;
-  secondFormGroup: FormGroup;
+  public user: IPayer = {};
 
-  constructor(private _formBuilder: FormBuilder) {}
+ public payPalConfig: IPayPalConfig;
 
   ngOnInit() {
-    this.firstFormGroup = this._formBuilder.group({
-      firstCtrl: ['', Validators.required]
-    });
-    this.secondFormGroup = this._formBuilder.group({
-      secondCtrl: ['', Validators.required]
-    });
+     this.initPaypalCheckout();
+  }
+
+  private initPaypalCheckout(): void {
+    this.payPalConfig = {
+      currency: 'USD',
+      clientId: 'sb',
+      createOrderOnClient: (data) => <ICreateOrderRequest> {
+        intent: 'CAPTURE',
+        //payer: this.user,
+        purchase_units: [{
+          amount: {
+            currency_code: 'USD',
+            value: '9.99',
+            breakdown: {
+              item_total: {
+                currency_code: 'USD',
+                value: '9.99'
+              }
+            }
+          },
+          items: [{
+            description: '',
+            name: 'Key Chain',
+            quantity: '1',
+            category: 'PHYSICAL_GOODS',
+            unit_amount: {
+              currency_code: 'USD',
+              value: '9.99'
+            },
+          }]
+        }]
+      },
+      advanced: {
+        commit: 'true'
+      },
+      style: {
+        label: 'checkout',
+        layout: 'vertical'
+      },
+      onApprove: (data, actions) => {
+        console.log('onApprove - transaction was approved, but not authorized', data, actions);
+        actions.order.get().then(details => {
+          console.log('onApprove - you can get full order details inside onApprove: ', details);
+        });
+      },
+      onClientAuthorization: (data) => {
+        console.log('onClientAuthorization - you should probably inform your server about completed transaction at this point', data);
+      },
+      onShippingChange: (data, actions) => {
+
+      },
+      onCancel: (data, actions) => {
+        console.log('OnCancel', data, actions);
+      },
+      onError: err => {
+        console.log('OnError', err);
+      },
+      onClick: (data, actions) => {
+        console.log('onClick', data, actions);
+      },
+    };
   }
 
 }
--- ImagenesRivera.Products.KeyChain/src/app/@order/order.module.ts
+++ ImagenesRivera.Products.KeyChain/src/app/@order/order.module.ts
@@ -1,23 +1,21 @@
+import { HttpClientModule } from '@angular/common/http';
 import { OrderService } from './order.service';
 import { NgModule } from '@angular/core';
 import { OrderComponent } from './order.component';
 import { SharedModule } from '../shared/shared.module';
 import { NgxPayPalModule } from 'ngx-paypal';
-
-import { PaymentComponent } from './payment/payment.component';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    OrderComponent,
-    PaymentComponent
+    OrderComponent
   ],
   exports: [OrderComponent],
   imports: [
     SharedModule,
     NgxPayPalModule,
     FormsModule,
-    ReactiveFormsModule
+    HttpClientModule
   ],
   providers:[OrderService]
 })
